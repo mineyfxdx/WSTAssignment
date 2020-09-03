c6f517fad7b94511d6355e84e394bf5a7418846d
Syam Rush <59945972+mineyfxdx@users.noreply.github.com>
2020-09-03 19:56:29 +0800

    Create indexProduct.php

---
+++ managementProduct/indexProduct.php
@@ -0,0 +1,7 @@
+<h1>Product Menu</h1>
+<ul>
+    <li><a href="viewProduct.php">View All Product</a><br>
+    <li><a href="addProduct.php">Add New Product</a><br>
+    <li><a href="../CustomerLoad">Update/Delete Product</a><br>
+    <li><a href="../managementIndex.php">Back To Main Menu</a><br>
+</ul>
